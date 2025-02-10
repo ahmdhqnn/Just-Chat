8315af00ebdec87cd98696b8ed0a2b41665be764
ahmdhqnn <ahmadhaqinn@gmail.com>
2025-02-10 09:40:46 +0700

    second commit

---
+++ src/routes/dashboard/DashboardPage.jsx
@@ -0,0 +1,9 @@
+import './dashboardPage.css'
+
+const DashboardPage = () => {
+    return (
+        <div className='dashboardPage'>dashboardPage</div>
+    )
+}
+
+export default DashboardPage
---
+++ src/routes/home/HomePage.jsx
@@ -0,0 +1,8 @@
+import './homePage.css'
+
+const HomePage = () => {
+    return (
+        <div className='homePage'></div>
+    )
+}
+export default HomePage
