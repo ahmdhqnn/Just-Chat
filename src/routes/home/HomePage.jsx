f2c16ce86041993128b52d34b51919b49fa8c236
ahmdhqnn <ahmadhaqinn@gmail.com>
2025-02-18 13:37:28 +0700

    homepage clerk

--- src/routes/home/HomePage.jsx
+++ src/routes/home/HomePage.jsx
@@ -4,7 +4,22 @@ import './homePage.css'
 const HomePage = () => {
     return (
         <div className='homePage'>
-            <Link to='/dashboard'>Dashboard</Link>
+            <img src='/' alt='' className='orbital'/>
+            <div className='left'>
+                <h1>Just Chat</h1>
+                <h2>Supercharge your craativity and productivity</h2>
+                <h3>lorem ipsum dolor sit amet conaaf dafagrgs gsewgsgs sggsgsg, sfsg gsgsgsewtwgcw twtw.</h3>
+                <Link to='/dashboard'>Get Started</Link>
+            </div>
+            <div className='right'>
+                <div className='imgContainer'>
+                    <div className='bgContainer'>
+                        <div className='bg'>
+                        </div>
+                        <img src='/bot.png' alt=''/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
