67cd9bf1d49610aa47b3136bf6087b8dfb95bce8
Ahmad Hakin Najili <ahmadhaqinn@gmail.com>
2025-03-03 23:49:03 +0700

    chatgpt clone design

--- src/routes/dashboard/DashboardPage.jsx
+++ src/routes/dashboard/DashboardPage.jsx
@@ -2,7 +2,18 @@ import './dashboardPage.css'
 
 const DashboardPage = () => {
     return (
-        <div className='dashboardPage'>dashboardPage</div>
+        <div className='dashboardPage'>
+            <div className='texts'>
+                <div className='logo'>
+                    <img src='/logo.png' alt='' />
+                    <h1>Just Chat</h1>
+                    <div>
+                        <div className='options'></div>
+                    </div>
+                    <div className='formContainer'></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
