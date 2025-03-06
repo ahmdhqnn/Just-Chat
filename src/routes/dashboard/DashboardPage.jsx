89edb517ba1116e87b9a85e3440c0e7d92cea211
Ahmad Hakin Najili <ahmadhaqinn@gmail.com>
2025-03-06 23:20:39 +0700

    chatgpt clone design

--- src/routes/dashboard/DashboardPage.jsx
+++ src/routes/dashboard/DashboardPage.jsx
@@ -7,11 +7,29 @@ const DashboardPage = () => {
                 <div className='logo'>
                     <img src='/logo.png' alt='' />
                     <h1>Just Chat</h1>
-                    <div>
-                        <div className='options'></div>
-                    </div>
-                    <div className='formContainer'></div>
                 </div>
+                <div className='options'>
+                    <div className='optiom'>
+                        <img src='/chat.png' alt='' />
+                        <span>Create a New Chat</span>
+                    </div>
+                    <div className='optiom'>
+                        <img src='/image.png' alt='' />
+                        <span>Analize Images</span>
+                    </div>
+                    <div className='optiom'>
+                        <img src='/code.png' alt='' />
+                        <span>Help me with my Code</span>
+                    </div>
+                </div>
+            </div>
+            <div className='formContainer'>
+                <form>
+                    <input type='text' placeholder='Ask me anything...'/>
+                    <button>
+                        <img src='/arrow.png' alt='' />
+                    </button>
+                </form>
             </div>
         </div>
     )
